c072dae103b1edc658b9fa26ea0a866258d6942a
Lucas Ribeiro <lucasribeirosantos.dev@gmail.com>
2025-05-20 15:58:56 -0300

    image animation fix 1.0

--- js/image.js
+++ js/image.js
@@ -1,30 +1,37 @@
-document.querySelectorAll('.project-img').forEach((img)=> {
-    const images = img.dataset.imgs.split(",")
-    let index = 1
-    let interval
+const base = window.location.pathname.includes("lucasribeirosantos-portfolio")
+  ? "/lucasribeirosantos-portfolio/"
+  : "/";
 
-    function changeImage (){
-        img.style.opacity = 0
-        setTimeout(() => {
-            img.src = images[index]
-            img.style.opacity = 1
-            index = (index + 1) % images.length
-            if(index === 0) { index = 1 }
-        }, 250);
-    }
+document.querySelectorAll('.project-img').forEach((img) => {
+  let images = img.dataset.imgs.split(",").map(src => {
+    return src.startsWith("/") ? src : base + src;
+  });
 
-    img.addEventListener('mouseenter', ()=>{
-        index = 1
-        changeImage()
-        interval = setInterval(changeImage, 1500)
-    })
+  let index = 1;
+  let interval;
 
-    img.addEventListener('mouseleave', ()=>{
-        clearInterval(interval)
-        img.style.opacity = 0
-        setTimeout(() => {
-            img.src = images[0]
-            img.style.opacity = 1
-        }, 250);
-    })
-})
+  function changeImage() {
+    img.style.opacity = 0;
+    setTimeout(() => {
+      img.src = images[index];
+      img.style.opacity = 1;
+      index = (index + 1) % images.length;
+      if (index === 0) index = 1;
+    }, 250);
+  }
+
+  img.addEventListener('mouseenter', () => {
+    index = 1;
+    changeImage();
+    interval = setInterval(changeImage, 1500);
+  });
+
+  img.addEventListener('mouseleave', () => {
+    clearInterval(interval);
+    img.style.opacity = 0;
+    setTimeout(() => {
+      img.src = images[0];
+      img.style.opacity = 1;
+    }, 250);
+  });
+});
